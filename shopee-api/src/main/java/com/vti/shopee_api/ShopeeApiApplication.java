a59cca7d28b0faad76dc9f9abdec38c7efeb7521
dinhvandong <dongvan.ict3@gmail.com>
2025-01-07 15:39:29 +0700

    update code
    first commit

---
+++ shopee-api/src/main/java/com/vti/shopee_api/ShopeeApiApplication.java
@@ -0,0 +1,13 @@
+package com.vti.shopee_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopeeApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopeeApiApplication.class, args);
+	}
+
+}
---
+++ shopee-api/src/test/java/com/vti/shopee_api/ShopeeApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vti.shopee_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopeeApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
